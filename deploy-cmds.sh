5b0c2a963d4148bf0646d75427e64406cd3eed1f
Felix C <felix.capuano@gmail.com>
2024-08-13 13:38:44 +0200

    proxy working

--- deploy-cmds.sh
+++ deploy-cmds.sh
@@ -1,10 +1,13 @@
-kubectl create  -f services/proxy/traefik.service.yml \
-                -f services/proxy/traefik-dashboard.service.yml \
-                -f services/proxy/traefik.deployment.yml \
-                -f services/proxy/traefik.role.yml \
-                -f services/proxy/traefik.rolebinding.yml \
-                -f services/proxy/traefik.account.yml
+export DEPLOY_ACTION="create"
 
-kubectl create  -f services/test/test.deployment.yml \
+# kubectl $DEPLOY_ACTION  -f services/proxy/traefik.service.yml \
+#                 -f services/proxy/traefik-dashboard.service.yml \
+#                 -f services/proxy/traefik.deployment.yml \
+#                 -f services/proxy/traefik.role.yml \
+#                 -f services/proxy/traefik.rolebinding.yml \
+#                 -f services/proxy/traefik.account.yml \
+#                 -f services/proxy/traefik.ingress.yml
+
+kubectl $DEPLOY_ACTION  -f services/test/test.deployment.yml \
                 -f services/test/test.ingress.yml \
                 -f services/test/test.service.yml
